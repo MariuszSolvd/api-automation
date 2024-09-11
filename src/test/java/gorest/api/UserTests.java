7991dc3e2bb4bb11d7a0a94a24a78b4a5e676d84
MariuszSolvd <mkaminski@solvd.com>
2024-09-11 16:38:30 +0200

    Implemented 2nd test case

--- src/test/java/gorest/api/UserTests.java
+++ src/test/java/gorest/api/UserTests.java
@@ -1,6 +1,7 @@
 package gorest.api;
 
 import com.solvd.dto.CreateUser;
+import com.solvd.model.User;
 import com.solvd.service.UserCreator;
 import com.solvd.utilis.ConfigLoader;
 import io.restassured.RestAssured;
@@ -39,6 +40,38 @@ public class UserTests {
                 .header("Content-Type", equalTo ("application/json; charset=utf-8"))
                 .header("Server", equalTo("cloudflare"))
                 .statusCode(201);
+    }
+
+    @Test
+    public void getUserById() {
+        //Creating random user
+        CreateUser createUser = UserCreator.createUser();
+
+        //Adding createUser
+        User user = given()
+                .auth().oauth2(ConfigLoader.getProperty("token"))
+                .contentType(ContentType.JSON)
+                .body(createUser)
+                .when()
+                .post()
+                .then()
+                .statusCode(201)
+                .extract()
+                .as(User.class);
+
+        //Getting added user along with assertion
+        given()
+                .auth().oauth2(ConfigLoader.getProperty("token"))
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/" + user.id())
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(user.id().intValue())).body("email", equalTo(user.email()))
+                .body("name", equalTo(user.name())).body("gender", equalTo(user.gender().getGender()))
+                .body("status", equalTo(user.status().getStatus()))
+                .header("Content-Type", equalTo ("application/json; charset=utf-8"))
+                .header("Server", equalTo("cloudflare"));
     }
 
 }
